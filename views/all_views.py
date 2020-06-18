48b3a02f62fa32c97456b739b05d06f71e103f5c
FernandoHernandezVazquez <1719210003@utectulancingo.edu.mx>
2020-06-18 00:08:28 +0000

    estilo formulario

--- views/all_views.py
+++ views/all_views.py
@@ -8,18 +8,18 @@ from pyramid.view import view_config
 @view_config(route_name='intro')
 def home_page(request):
     boot='<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">'
-    header = '<p class="text-light bg-dark">MI PRIMER FORMULARIO</p>'
+    header = '<h1 class="text-light bg-dark">MI PRIMER FORMULARIO</h1>'
 
-    body = '<form> <div class="row"> <div class="col"> <input type="number" class="form-control" placeholder="Matricula"> </div> <div class="col"> <input type="text" class="form-control" placeholder="Nombre"> </div> </div></form><div class="row"> <div class="col"> <input type="text" class="form-control" placeholder="Primer Apellido"> </div><div class="col"> <input type="text" class="form-control" placeholder="Segundo Apellido"></div><div class="col"> <input type="text" class="form-control" placeholder="Segundo Apellido"></div>'
+    body = '<form><div class="form-group"><label for="exampleFormControlInput1">MATRICULA</label><input type="number" class="form-control" id="ControlInput1" placeholder="17180000"></div><div class="form-group"><label for="exampleFormControlInput1">NOMBRE</label><input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></div><div class="form-group"><label for="exampleFormControlInput1">PRIMER APELLIDO</label><input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></div><div class="form-group"><label for="exampleFormControlInput1">SEGUNDO APELLIDO</label><input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></div><div class="form-group"><label for="exampleFormControlInput1">EDAD</label><input type="number" class="form-control" id="exampleFormControlInput1" placeholder="18"></div><div class="form-group"><label for="exampleFormControlInput1">FECHA DE NACIMIENTO</label><input type="date" class="form-control" id="exampleFormControlInput1" placeholder=""></div><div><select class="form-control form-control"><option>Hombre</option><option>Mujer</option><option>Otro</option></select></div> <div><label for="validationDefault04">Estado</label><select class="custom-select" id="validationDefault04" required><option selected disabled value="">Choose...</option><option>...</option></select></div><br><br>'
     
     body += '<body background="https://image.freepik.com/vector-gratis/fondo-formas-abstractas-triangulo-azul_1035-17551.jpg">'
 
-    button='<br><button type="button" class="btn btn-primary">Primary</button>'
+    button='<a class="btn btn-danger" href="/fer" role="button">GUARDAR</a>'
 
     # En la etiqueta 'a', observe que href contiene '/ fer', esta ruta se definirá en el archivo intro.py
     # Simplemente le dice a la vista que navegue a esa ruta y ejecute cualquier código que esté en esa vista
 
-    return Response(boot + header +  body + button)
+    return Response(boot + header +  body + button )
 
 
 
